cbb42bde92e2084daabd863cb932aa5f5ae6605f
Yan Kong <quic_xiankong@quicinc.com>
2025-03-29 21:32:05 +0800

    telaf: Add TAF_PA_WEAK for voice call default PA
    Change-Id: I3de852afefcfe2ec870f0c4aee3dff262c735a46

--- include/taf_pa_voicecall.h
+++ include/taf_pa_voicecall.h
@@ -6,6 +6,12 @@
 #ifndef TAF_PA_VOICECALL_H
 #define TAF_PA_VOICECALL_H
 
+#ifdef TAF_PA_DEFAULT
+#define TAF_PA_WEAK __attribute__((weak))
+#else
+#define TAF_PA_WEAK
+#endif
+
 #define PA_MAX_DESTINATION_LEN_BYTE 51
 
 typedef enum
@@ -35,44 +41,35 @@ typedef enum
 
 typedef struct taf_pa_voicecall_Ref* taf_pa_voicecall_Ref_t;
 
-struct taf_pa_voicecall_CallInfo_t
-{
-    int8_t phoneId;
-    char destId[PA_MAX_DESTINATION_LEN_BYTE];
-    taf_pa_voicecall_dir_t direction;
-    taf_pa_voicecall_Ref_t reference;
-    le_dls_Link_t link;
-};
-
 typedef void (*taf_pa_voicecall_CallCb)(taf_pa_voicecall_Ref_t reference, le_result_t result, void *contextPtr);
 
 typedef void (*taf_pa_voicecall_EventListener)(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_event_t event, void *contextPtr);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_Init();
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_Init();
 
-LE_SHARED __attribute__((weak)) taf_pa_voicecall_Ref_t taf_pa_voicecall_CreateReference(int8_t phoneId, const char *destId, taf_pa_voicecall_dir_t direction);
+LE_SHARED TAF_PA_WEAK taf_pa_voicecall_Ref_t taf_pa_voicecall_CreateReference(int8_t phoneId, const char *destId, taf_pa_voicecall_dir_t direction);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_DeleteReference(taf_pa_voicecall_Ref_t reference);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_DeleteReference(taf_pa_voicecall_Ref_t reference);
 
-LE_SHARED __attribute__((weak)) int8_t taf_pa_voicecall_GetCallPhoneId(taf_pa_voicecall_Ref_t reference);
+LE_SHARED TAF_PA_WEAK int8_t taf_pa_voicecall_GetCallPhoneId(taf_pa_voicecall_Ref_t reference);
 
-LE_SHARED __attribute__((weak)) taf_pa_voicecall_dir_t taf_pa_voicecall_GetCallDirection(taf_pa_voicecall_Ref_t reference);
+LE_SHARED TAF_PA_WEAK taf_pa_voicecall_dir_t taf_pa_voicecall_GetCallDirection(taf_pa_voicecall_Ref_t reference);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_GetCallDestination(taf_pa_voicecall_Ref_t reference, char *destinationPtr, uint32_t destSize);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_GetCallDestination(taf_pa_voicecall_Ref_t reference, char *destinationPtr, uint32_t destSize);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_Make(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_Make(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_Stop(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_Stop(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_Hold(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_Hold(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_Resume(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_Resume(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_Answer(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_Answer(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_Swap(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_Swap(taf_pa_voicecall_Ref_t reference, taf_pa_voicecall_CallCb callback, void* contextPtr);
 
-LE_SHARED __attribute__((weak)) le_result_t taf_pa_voicecall_RegisterEventListener(taf_pa_voicecall_EventListener listener, void* contextPtr);
+LE_SHARED TAF_PA_WEAK le_result_t taf_pa_voicecall_RegisterEventListener(taf_pa_voicecall_EventListener listener, void* contextPtr);
 
 #endif /* TAF_PA_VOICECALL_H */
 
