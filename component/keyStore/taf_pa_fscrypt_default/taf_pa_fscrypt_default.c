6b1e60c55350a92b696bbcb40e8d7e42abf5bccf
Andrew Zhao <zhax@qti.qualcomm.com>
2025-04-11 17:18:16 +0800

    telaf: Add default PA for keyStore Service
    Change-Id: I7e7910829f069d7c68b5bbbf4105b7dfa7a46416
    Signed-off-by: Andrew Zhao <zhax@qti.qualcomm.com>

---
+++ component/keyStore/taf_pa_fscrypt_default/taf_pa_fscrypt_default.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include "legato.h"
+#include "interfaces.h"
+#include "taf_pa_fscrypt.h"
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * PA initialization.
+ */
+//--------------------------------------------------------------------------------------------------
+void taf_pa_fsc_Init
+(
+    void* cryptoFunc
+)
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Get a key file reference by directory name.
+ */
+//--------------------------------------------------------------------------------------------------
+le_result_t taf_pa_fsc_GetKey
+(
+    le_msg_SessionRef_t clientSessionRef,   ///< [IN] Client session reference
+    const char* dirName,                    ///< [IN] dir Name
+    KeyMgt_KeyFileRef_t* keyFileRefPtr,     ///< [OUT] Key file reference.
+    uint8_t* key,                           ///< [OUT] Raw key
+    size_t keyLen                           ///< [OUT] Length of raw key
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Create AES key and return a key file reference.
+ */
+//--------------------------------------------------------------------------------------------------
+le_result_t taf_pa_fsc_GenerateAesKey
+(
+    le_msg_SessionRef_t clientSessionRef,   ///< [IN] Client session reference
+    const char* dirName,                    ///< [IN] dir Name
+    KeyMgt_KeyFileRef_t* keyFileRefPtr,     ///< [OUT] Key file reference
+    uint8_t* key,                           ///< [OUT] Raw key
+    size_t keyLen                           ///< [OUT] Length of raw key
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Delete a key file.
+ */
+//--------------------------------------------------------------------------------------------------
+le_result_t taf_pa_fsc_DeleteKey
+(
+    le_msg_SessionRef_t clientSessionRef, ///< [IN] Client session reference
+    KeyMgt_KeyFileRef_t keyFileRef        ///< [IN] Key file reference
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * The PA initialization function.
+ */
+//--------------------------------------------------------------------------------------------------
+COMPONENT_INIT
+{
+    LE_INFO("Telaf fscrypt default PA initialized.");
+}
