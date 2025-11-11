0811315ad5bac6454a7c216ede27deaf8a664108
Jaishika Joshi <jaishika@qti.qualcomm.com>
2025-11-11 09:55:22 +0530

    tafHMSvc: Add default PA for Health Monitor Service
    Change-Id: I7e8eca50931badaea487c6c6b9de260ad76e57b1

---
+++ component/taf_pa_health/taf_pa_health.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include "taf_pa_health.hpp"
+
+pa_result_t taf_pa_health_ModemNotificationInit(void)
+{
+    PA_INFO("Default platform adatper PA_NOT_IMPLEMENTED");
+    return PA_NOT_IMPLEMENTED;
+}
+
+pa_result_t taf_pa_health_RegModemListener(void)
+{
+    PA_INFO("Default platform adatper PA_NOT_IMPLEMENTED");
+    return PA_NOT_IMPLEMENTED;
+}
+
+pa_result_t taf_pa_health_DeregModemListener(void)
+{
+    PA_INFO("Default platform adatper PA_NOT_IMPLEMENTED");
+    return PA_NOT_IMPLEMENTED;
+}
+
+pa_result_t taf_pa_health_PhoneInit(void)
+{
+    PA_INFO("Default platform adatper PA_NOT_IMPLEMENTED");
+    return PA_NOT_IMPLEMENTED;
+}
+
+pa_result_t taf_pa_health_ReqPhoneOperatingMode(void)
+{
+    PA_INFO("Default platform adatper PA_NOT_IMPLEMENTED");
+    return PA_NOT_IMPLEMENTED;
+}
+
+pa_result_t taf_pa_health_RegModemOperationModeUpdateHandler
+(
+    taf_pa_health_ModemOperatingModeUpdateHandler_t handlerFunc
+)
+{
+    PA_INFO("Default platform adatper PA_NOT_IMPLEMENTED");
+    return PA_NOT_IMPLEMENTED;
+}
+
+pa_result_t taf_pa_health_RegModemStatusUpdateHandler
+(
+    taf_pa_health_ModemStatusUpdateHandler_t handlerFunc
+)
+{
+    PA_INFO("Default platform adatper PA_NOT_IMPLEMENTED");
+    return PA_NOT_IMPLEMENTED;
+}
